7ef8a17d8467fc015a4cb349dda4de885d2b8ab9
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-16 23:51:42 +0200

    V0.2 - Pruebas html, correccion de errores

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,4 @@
-const nav = document.querySelector("#nav");
+/*const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
@@ -9,3 +9,28 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+const verProductoBtn = document.querySelector(".verProducto");
+
+verProductoBtn.addEventListener("click", () => {
+    window.location.href = "nueva-url.html";
+});
+    */  
+document.addEventListener("DOMContentLoaded", function() {
+const productos = document.querySelectorAll(".producto");
+
+productos.forEach(producto => {
+    const verProductoBtn = producto.querySelector(".verProducto");
+    verProductoBtn.addEventListener("click", () => {
+        const imagen = producto.querySelector(".imagen").getAttribute("src");
+        const titulo = producto.querySelector(".titulo").textContent;
+        const precio = producto.querySelector(".precio span").textContent;
+
+        // Generar la URL de la página de detalle con los datos del producto
+        const urlDetalle = `pagina1.html`;
+
+        // Abrir la página de detalle en una nueva pestaña o ventana
+        window.open(urlDetalle, "_blank");
+    });
+});
+});
