17cea681e018be2a818d25ac25a8e6493713c7e2
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-18 13:26:54 +0200

    V0.6 - Haciendo carrito - carrito 2.0

--- js/detalle.js
+++ js/detalle.js
@@ -1,27 +1,66 @@
 let totalProductosCarrito = [];
-// Desglosamos datos del producto seleccionado en el catálogo
+
+//Desglosamos datos del producto seleccionado en el catálogo
 const urlProducto = new URLSearchParams(window.location.search);
 const titulo = urlProducto.get("titulo");
 const precio = urlProducto.get("precio");
 const imagen = decodeURIComponent(urlProducto.get("imagen"));
-const ref    = urlProducto.get("ref")
-const cantidadTotal = urlProducto.get("cantidadTotal");
-const totalProductos = urlProducto.get("totalProductos");
+const ref = urlProducto.get("ref");
+let cantidadTotal = parseFloat(urlProducto.get("cantidadTotal"));
+let totalProductos = parseInt(urlProducto.get("totalProductos"));
 
-// Obtenemos los elementos del DOM usando clases
+//Obtenemos los elementos del DOM usando clases
 const tituloElement = document.querySelector(".titulo");
-const precioElement = document.querySelector(".precio");
+const precioElement = document.querySelector(".precioLimpio");
 const imagenElement = document.querySelector(".imagen");
-const refElement    = document.querySelector(".ref");
+const refElement = document.querySelector(".ref");
 const cantidadTotalElement = document.querySelector(".cantidad");
 const totalProductosElement = document.querySelector(".totalProductos");
 
-// Mostramos los parámetros en el HTML
+//Mostramos los parámetros en el HTML
 tituloElement.textContent = titulo;
 precioElement.textContent = precio;
 imagenElement.src = imagen;
 refElement.textContent = ref;
-cantidadTotalElement.textContent = parseFloat(cantidadTotal).toFixed(2);
-totalProductosElement.textContent = parseInt(totalProductos);
+cantidadTotalElement.textContent = cantidadTotal.toFixed(2);
+totalProductosElement.textContent = totalProductos;
 
+//Obtiene referencia del botón
+let botonAnadir = document.getElementById("anadirProducto");
+
+//Añade evento "click" al botón para obtener el valor del input value
+botonAnadir.addEventListener("click", () => {
+  let inputCantidad = document.getElementById('cantidad');
+  let cantidad = parseInt(inputCantidad.value);
+  actualizarCarrito(cantidad);
+});
+
+//Añade cantidad seleccionada y calcula el precio final, teniendo en cuenta
+//las unidades que hay en el carrito y el precio ya calculado de estas.
+function actualizarCarrito(cantidad) {
+  let precioElement = document.querySelector('.precioLimpio');
+  let precio = precioElement.textContent;
+  console.log(precio);
+  cantidadTotal += (precio * cantidad);
+  console.log(cantidadTotal);
+  totalProductos += cantidad;
+
+  cantidadTotalElement.textContent = cantidadTotal.toFixed(2);
+  totalProductosElement.textContent = totalProductos;
+}
+
+const carritoURL = document.getElementById("volver");
+
+//Recorre los objetos producto del html, añade evento "click" a cada uno y guarda parámetros en url
+    carritoURL.addEventListener("click", () => {
+      console.log("check volver");
+      const unidades = document.querySelector(".totalProductos").textContent;
+      const precio = document.querySelector(".precioLimpio").textContent;
+      //Pasa datos del producto a una nueva pestaña
+      const urlCatalogo = `catalogo.html?totalProductos=${unidades}&cantidadTotal=${precio}`;
+      console.log(urlCatalogo);
+      window.location.href = urlCatalogo;
+    });
+
+ 
 
--- js/navigation.js
+++ js/navigation.js
@@ -18,13 +18,13 @@ cerrar.addEventListener("click", () => {
 //Toma todos los elementos de los objetos con clase producto
 const productosURL = document.querySelectorAll(".producto");
     
-    //recorre los objetos producto del html, añade evento "click" a cada imagen y guarda parametros en url
+//recorre los objetos producto del html, añade evento "click" a cada imagen y guarda parametros en url
 productosURL.forEach(producto => {
     const verProductoBtn = producto.querySelector(".imagen");
     verProductoBtn.addEventListener("click", () => {
         const imagen = producto.querySelector(".imagen").getAttribute("src");
         const titulo = producto.querySelector(".titulo").textContent;
-        const precio = producto.querySelector(".precio").textContent;
+        const precio = producto.querySelector(".precioLimpio").textContent;
         const ref = producto.querySelector(".ref").textContent;
         //Pasa datos producto a nueva pestaña
         const urlDetalle = `detalle.html?imagen=${encodeURIComponent(imagen)}&titulo=${encodeURIComponent(titulo)}&precio=${encodeURIComponent(precio)}&ref=${encodeURIComponent(ref)}&totalProductos=${totalProductos}&cantidadTotal=${cantidad}`;
@@ -36,15 +36,20 @@ productosURL.forEach(producto => {
 //Añadimos elementos con las clases especificadas a unas constantes
 const carrito = document.querySelector(".totalProductos");
 const cantidadTotal = document.querySelector(".cantidad");
-const productosCarrito = document.querySelectorAll(".producto");   
+const productosCarrito = document.querySelectorAll(".producto"); 
 
-//Función para actualizar el contador de productos y mostrar los productos en el carrito
-function actualizarCarrito() {
-  //Actualiza el contador de productos
-  carrito.textContent = totalProductos;
-  //Actualiza cantidad total y se limita a dos decimales.
-  cantidadTotal.textContent = cantidad.toFixed(2);
-}
+//Recorre objetos producto, añade evento "click" a cada boton y guarda parametros en una variable
+//ejecuta lacuncion añadirProducto con esas variables creadas.
+productosCarrito.forEach(producto => {
+  const añadirBtn = producto.querySelector(".anadirProducto");
+  añadirBtn.addEventListener("click", () => {
+    const imagen = producto.querySelector(".imagen").getAttribute("src");
+    const titulo = producto.querySelector(".titulo").textContent;
+    const precio = producto.querySelector(".precioLimpio").textContent;
+    const ref = producto.querySelector(".ref").textContent;
+    añadirProducto(imagen, titulo, precio, ref);
+  });
+});
 
 //Función para añadir un producto al carrito
 function añadirProducto(imagen, titulo, precio, ref) {
@@ -57,16 +62,26 @@ function añadirProducto(imagen, titulo, precio, ref) {
   actualizarCarrito();
 }
 
-//recorre objetos producto, añade evento "click" a cada boton y guarda parametros en una variable
-productosCarrito.forEach(producto => {
-  const añadirBtn = producto.querySelector(".anadirProducto");
-  añadirBtn.addEventListener("click", () => {
-    const imagen = producto.querySelector(".imagen").getAttribute("src");
-    const titulo = producto.querySelector(".titulo").textContent;
-    const precio = producto.querySelector(".precio").textContent;
-    const ref = producto.querySelector(".ref").textContent;
-    añadirProducto(imagen, titulo, precio, ref);
-  });
-});
+//Función para actualizar el contador de productos y mostrar los productos en el carrito
+function actualizarCarrito() {
+  //Actualiza el contador de productos
+  carrito.textContent = totalProductos;
+  //Actualiza cantidad total y se limita a dos decimales.
+  cantidadTotal.textContent = cantidad.toFixed(2);
+}
 
+function desdeDetalle(){
+// Desglosamos datos del producto seleccionado en el catálogo
+const urlProducto = new URLSearchParams(window.location.search);
+let cantidadTotal2 = parseFloat(urlProducto.get("precio"));
+let totalProductos2 = parseInt(urlProducto.get("unidades"));
+
+// Obtenemos los elementos del DOM usando clases
+const cantidadTotalElement = document.querySelector(".cantidad");
+const totalProductosElement = document.querySelector(".totalProductos");
+
+// Mostramos los parámetros en el HTML
+cantidadTotalElement.textContent = cantidadTotal2.toFixed(2);
+totalProductosElement.textContent = totalProductos2;
+}
     
