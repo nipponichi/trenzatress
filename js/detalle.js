1f6b6d60074256239d39689a817252fa68a2300b
Javier Lopez <javier.lopez.sola@gmail.com>
2023-06-19 22:12:06 +0200

    V0.7.1 - Comentarios JS revisados

--- js/detalle.js
+++ js/detalle.js
@@ -1,9 +1,11 @@
-let totalProductosCarrito = [];
 
-//Desglosamos datos del producto seleccionado en el catálogo
+//Desglosamos datos del producto seleccionado en el catálogo utilizando la URL
 const urlProducto = new URLSearchParams(window.location.search);
+
+//Se localizan los diferentes parametros y se asignan a unas constantes
 const titulo = urlProducto.get("titulo");
 const precio = urlProducto.get("precio");
+//La imagen necesita ser decodificada
 const imagen = decodeURIComponent(urlProducto.get("imagen"));
 const ref = urlProducto.get("ref");
 let cantidadTotal = parseFloat(urlProducto.get("cantidadTotal"));
@@ -25,20 +27,23 @@ refElement.textContent = ref;
 cantidadTotalElement.textContent = cantidadTotal.toFixed(2);
 totalProductosElement.textContent = totalProductos;
 
-//Obtiene referencia del botón
+//Obtiene referencia del botón Añadir Producto
 let botonAnadir = document.getElementById("anadirProducto");
 
-//Añade evento "click" al botón para obtener el valor del input value
+//Añade evento "click" al botón para obtener el valor del input "cantidad"
 botonAnadir.addEventListener("click", () => {
   let inputCantidad = document.getElementById('cantidad');
   let cantidad = parseInt(inputCantidad.value);
+  //Llama a la función actualizarCarrito con el valor de "cantidad"
   actualizarCarrito(cantidad);
 });
 
 //Añade cantidad seleccionada y calcula el precio final, teniendo en cuenta
 //las unidades que hay en el carrito y el precio ya calculado de estas.
 function actualizarCarrito(cantidad) {
+  //Extrae el precio de la etiqueta con la clase correspondiente
   let precioElement = document.querySelector('.precioLimpio');
+  //Extraemos el "texto" y lo pasamos a precio para poder operar con el valor
   let precio = precioElement.textContent;
   console.log(precio);
   cantidadTotal += (precio * cantidad);
@@ -49,8 +54,9 @@ function actualizarCarrito(cantidad) {
   totalProductosElement.textContent = totalProductos;
 }
 
-const carritoURL = document.getElementById("volver");
 
+/* Código no funcional
+const carritoURL = document.getElementById("volver");
 //Recorre los objetos producto del html, añade evento "click" a cada uno y guarda parámetros en url
     carritoURL.addEventListener("click", () => {
       console.log("check volver");
@@ -62,5 +68,13 @@ const carritoURL = document.getElementById("volver");
       window.location.href = urlCatalogo;
 });
 
+let zoomImagen = document.querySelector(".imagen");
+
+//Añade etiqueta zoom al hacer click en la imagen
+zoomImagen.addEventListener("click",() => {
+  console.log("Check zoom");
+  zoomImagen.classList.add(".zoom");
+});
+*/
  
 
--- js/navigation.js
+++ js/navigation.js
@@ -7,6 +7,8 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
+//Hace visible un menu desplegable segun el ancho de pantalla marcado en 
+//el css (550px), se hace para dispositivos móviles.
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
 })
@@ -22,12 +24,14 @@ const productosURL = document.querySelectorAll(".producto");
 productosURL.forEach(producto => {
     const verProductoBtn = producto.querySelector(".imagen");
     verProductoBtn.addEventListener("click", () => {
+        //De la imagen tomamos la ruta para enviarla a traves de la variable
         let imagen = producto.querySelector(".imagen").getAttribute("src");
         let titulo = producto.querySelector(".titulo").textContent;
         let precio = producto.querySelector(".precioLimpio").textContent;
         let ref = producto.querySelector(".ref").textContent;
         //Pasa datos producto a nueva pestaña
         let urlDetalle = `detalle.html?imagen=${encodeURIComponent(imagen)}&titulo=${encodeURIComponent(titulo)}&precio=${encodeURIComponent(precio)}&ref=${encodeURIComponent(ref)}&totalProductos=${totalProductos}&cantidadTotal=${cantidad}`;
+        //devuelve el resultado en forma de url
         window.location.href = urlDetalle;
         });
     });
@@ -39,23 +43,30 @@ const cantidadTotal = document.querySelector(".cantidad");
 const productosCarrito = document.querySelectorAll(".producto"); 
 
 //Recorre objetos producto, añade evento "click" a cada boton y guarda parametros en una variable
-//ejecuta lacuncion añadirProducto con esas variables creadas.
+//ejecuta la función añadirProducto con esas variables creadas.
 productosCarrito.forEach(producto => {
   const añadirBtn = producto.querySelector(".anadirProducto");
   añadirBtn.addEventListener("click", () => {
+    //De la imagen tomamos la ruta para enviarla a traves de la variable
     let imagen = producto.querySelector(".imagen").getAttribute("src");
     let titulo = producto.querySelector(".titulo").textContent;
     let precio = producto.querySelector(".precioLimpio").textContent;
     let ref = producto.querySelector(".ref").textContent;
+    //Ejecuta esta función con los parametros cargados
     añadirProducto(imagen, titulo, precio, ref);
   });
 });
 
 //Función para añadir un producto al carrito
 function añadirProducto(imagen, titulo, precio, ref) {
-  const producto = { imagen, titulo, precio, ref };
+  let producto = { imagen, titulo, precio, ref };
+  //Añade productos a array, se hace para mas tarde recorrer el array
+  //en otra función y poder comparar la ref del producto y 
+  //no duplicar productos del mismo tipo en el carrito
   productosArray.push(producto);
+  //Toma de producto el valor precio, lo "entiende" como número decimal y lo suma a cantidad.
   cantidad += parseFloat(producto.precio);
+  //Añade +1 al total de productos del carrito.
   totalProductos++;
 
   //Actualiza el carrito
@@ -70,9 +81,11 @@ function actualizarCarrito() {
   cantidadTotal.textContent = cantidad.toFixed(2);
 }
 
+//Función para mantener los valores del carrito al volver a catalogo desde detalle
 function desdeDetalle(){
 //Desglosamos datos del producto seleccionado en el catálogo
 const urlProducto = new URLSearchParams(window.location.search);
+//Asignamos esos valores a unas variables
 let cantidadTotal2 = parseFloat(urlProducto.get("precio"));
 let totalProductos2 = parseInt(urlProducto.get("unidades"));
 
@@ -80,29 +93,34 @@ let totalProductos2 = parseInt(urlProducto.get("unidades"));
 let cantidadTotalElement = document.querySelector(".cantidad");
 let totalProductosElement = document.querySelector(".totalProductos");
 
-//Mostramos los parámetros en el HTML
+//Mostramos los parámetros en el HTML de cara al usuario
 cantidadTotalElement.textContent = cantidadTotal2.toFixed(2);
 totalProductosElement.textContent = totalProductos2;
 }
 
+//Se asegura que el contenido de la web esta completamente cargado.
 document.addEventListener('DOMContentLoaded', () => {
   const formulario = document.getElementById('formulario');
+  //Al clicar "enviar" (botón tipo 'submit') se ejecuta esta función flecha
   formulario.addEventListener('submit', (evento) => {
+    //Evitamos que el formulario se envíe de
+    //forma predeterminada y así se pueda ejecutar el código
     evento.preventDefault();
 
-    //Pasamos valores de campos a variables
+    //Pasamos valores de campos (clases html) a variables
     let nombre = document.querySelector('.nom').value;
     let telefono = document.querySelector('.tel').value;
     let apellidos = document.querySelector('.apel').value;
     let correo = document.querySelector('.correo').value;
-    let mensaje = document.querySelector('textarea').value;
+    let mensaje = document.querySelector('.reason').value;
 
-    //Verificamos si los campos están vacíos
+    //Verificamos si alguno de los campos está vacío
     if (!nombre || !telefono || !apellidos || !correo || !mensaje) {
       swal("Error","Completa los campos del formulario","warning");
       return;
     }
 
+    //Lanzamos este mensage si todos los campos contienen información
     swal("Perfecto","Formulario enviado correctamente","success");
     
     //Restablece los campos del formulario
